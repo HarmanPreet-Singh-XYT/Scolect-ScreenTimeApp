341505e95ba9db580d514231ac52075d64d4d8a4
Harmanpreet Singh <preetsinghharman27@gmail.com>
2026-02-25 01:42:04 -0500

    version update

--- lib/sections/controller/settings_data_controller.dart
+++ lib/sections/controller/settings_data_controller.dart
@@ -155,7 +155,7 @@ class SettingsManager {
   late Map<String, dynamic> settings;
 
   Map<String, String> versionInfo = {
-    "version": "2.0.6",
+    "version": "2.0.7",
     "type": "Stable Build"
   };
 
